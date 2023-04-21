cb5abce05b9f0c3cab7e3d386cba8e22005742a1
ваше_имя <grigorenko-marko@rambler.ru>
2023-04-21 23:10:54 +0300

    task02

--- task02/Program.cs
+++ task02/Program.cs
@@ -25,7 +25,7 @@ void FillArray (int[,] matr)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matr[i,j]=new Random().Next(1,10);
+            matr[i,j]=new Random().Next(1,100);
         }
     }
 }
