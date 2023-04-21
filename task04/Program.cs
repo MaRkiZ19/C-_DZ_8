8a981b10d658851cc6650f60920aefff9b86e23f
ваше_имя <grigorenko-marko@rambler.ru>
2023-04-22 00:00:41 +0300

    in progress task04

---
+++ task04/Program.cs
@@ -0,0 +1,42 @@
+﻿// .Сформируйте трёхмерный массив из случайных чисел.
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+//Массив размером 2 x 2 x 2
+
+
+
+int Num (string arg)
+{
+    System.Console.WriteLine($"Введите {arg} ");
+    return int.Parse(System.Console.ReadLine());
+}
+
+int n = Num("n столбцов");
+int m = Num("m строк");
+int q = Num("q глубину");
+
+int[,,] matrix = new int[m,n,q];
+
+void FillArray (int[,,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+           for (int q = 0; q < matr.GetLength(2); q++)
+           {
+              matr[i,j,q]=new Random().Next(1,100);
+           }
+        }
+    }
+}
+
+
+
+
+
+
+System.Console.WriteLine();
+FillArray(matrix);
+
+
+
